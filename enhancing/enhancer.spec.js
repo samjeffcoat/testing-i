7bf7d08e811f232d894d1f24878f3430cfe80fb2
samjeffcoat <samjeffcoat@gmail.com>
2019-03-11 15:35:31 -0500

    Durability checker works

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,13 +1,33 @@
 const enhancer = require('./enhancer.js');
 // test away!
 
+
+const weapon1= {
+    name: "Bear Claw",
+    durability: 70,
+    enhancement: 0
+}
+
+const weapon2 ={
+    name: "fiery cannon",
+    durability:100,
+    enhancement:25
+
+}
+
+
+
+
+
+
+
 describe('enhancer.js', () => {
     test('can run the tests', ()=> {
         expect(true).toBe(true);
     })
 })
 
-describe('repair()', ()=> {
+describe(' Testing enhancer.repair', ()=> {
     it('should return durability to be restored to 100', () => {
-expect(repair((1,2, 3))).toBe(100);
+expect(weapon2.durability).toEqual(100);
     })})

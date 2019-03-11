df8dba53359f4e4274bc9de282ce6e1d41246a54
samjeffcoat <samjeffcoat@gmail.com>
2019-03-11 17:56:33 -0500

    Succeed works but I am not sure it is right

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -9,17 +9,25 @@ function succeed(item) {
   //if successful enhancement increases by 1
   // if the enhancement level is 20, the enhancement level is not changed
   //durability remains the same
-  item.enhancement += 1;
-  if (item.enhancement < 20) item.enhancement = 20;
+
+  let enhancer = item.enhancement;
+  return {
+    ...item,
+    enhancement: enhancer + 1
+  };
+  /*
+  item.enhancement++;
+  if ((item.enhancement = 20)) return (item.enhancement = 20);
 
   return item;
+  */
 }
 
 function fail(item) {
   //if the items enhancementis less than 15, the durabiltity of the item is decreased by 5
   // if the items enhancement is 15 or more, the durability of the item is decreased by10
   // if the items enhancement level is greater than 16 the enhancement level decreases by 1
-
+  //if (item.enhancement < 15 item.durability-5)
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,28 +1,35 @@
-const enhancer = require('./enhancer.js');
+const { repair, fail, succeed } = require("./enhancer.js");
 // test away!
 
+const weapon1 = {
+  name: "Bear Claw",
+  durability: 70,
+  enhancement: 0
+};
 
-const weapon1= {
+const weapon2 = {
+  name: "fiery cannon",
+  durability: 100,
+  enhancement: 25
+};
+
+describe("enhancer.js", () => {
+  test("can run the tests", () => {
+    expect(true).toBe(true);
+  });
+});
+
+it("succeed() should increase enhancement by 1", () => {
+  const item = {
     name: "Bear Claw",
     durability: 70,
     enhancement: 0
-}
+  };
+  expect(succeed(weapon1).enhancement).toEqual(1);
+});
 
-const weapon2 ={
-    name: "fiery cannon",
-    durability:100,
-    enhancement:25
-
-}
-
-
-describe('enhancer.js', () => {
-    test('can run the tests', ()=> {
-        expect(true).toBe(true);
-    })
-})
-
-describe(' Testing enhancer.repair', ()=> {
-    it('should return durability to be restored to 100', () => {
-expect(weapon2.durability).toEqual(100);
-    })})
+describe(" Testing enhancer.repair", () => {
+  it("should return durability to be restored to 100", () => {
+    expect(weapon2.durability).toEqual(100);
+  });
+});
